993c1a126598dfc52320244b999c4c0375a2fbd4
AtchayaSelvam <atchayapanneerselvam1996@gmail.com>
2024-01-03 21:41:50 -0500

    adding files

---
+++ src/main/java/com/atchaya/LearnJava/App.java
@@ -0,0 +1,13 @@
+package com.atchaya.LearnJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
